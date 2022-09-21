6bc7ec2977738a1e9ad7e109fcda3f6bf62132c5
ALEKSANDAR <aleksandar.finki@gmail.com>
2022-09-21 14:33:09 +0200

    Added comment

--- src/main/java/mk/ukim/finki/wp/eshop/web/controller/HomeController.java
+++ src/main/java/mk/ukim/finki/wp/eshop/web/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// home page controller
 @Controller
 @RequestMapping(value = {"/", "/home"})
 public class HomeController {
